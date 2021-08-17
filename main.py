5f90f2c4923c4cbe4d020e96b2760bdc934dc1dc
guswls4928 <73646152+guswls4928@users.noreply.github.com>
2021-08-17 17:54:37 +0900

    Add files via upload
    complete but have little bug

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
+from threading import Thread
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import time
+import math
+
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
 
 MainAttractor_mass = 1000
 Attractor_mass = 1
@@ -9,21 +14,26 @@ Attractor_mass = 1
 class Attractor:
     
     def __init__(self):
-        self.x = 0
-        self.y = 0
         self.G = 1
-        self.mass = 1000
-        self.speed = int(input("행성의 속도를 입력하세요 : "))
+        self.mass = 100000
+        self.speed = 10
+        # int(input("행성의 속도를 입력하세요 : "))
+
+        self.x = 25
+        self.y = 225
 
 class Pointer:
     
     def __init__(self):
-        self.x = 0
-        self.y = 0
         self.G = 1
-        self.mass = 10
-
-class Mover(QWidget):
+        self.mass = 1
+        self.x = 400
+        self.y = 470
+        self.a = 0
+        self.xspeed = -1
+        self.yspeed = 0
+        
+class CWidget(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -31,10 +41,17 @@ class Mover(QWidget):
         self.attractor = Attractor()
     
         self.pointor = Pointer()
+        
+
+        self.thread = Thread(target=self.threadFunc)
+        self.bThread = False     
 
         self.initUI()
 
     def initUI(self):
+        self.setWindowTitle('swingby')        
+        self.bThread = True
+        self.thread.start()
         self.show()
 
     def paintEvent(self, e):
@@ -44,9 +61,6 @@ class Mover(QWidget):
         d=50
         r=d/2
 
-        self.attractor.x = r
-        self.attractor.y = self.rect().height()/2
-
         rect = QRectF(self.attractor.x-r, self.attractor.y-r, d, d)
         qp.setBrush(QColor(0,0,0,128))
         qp.drawEllipse(rect)
@@ -55,9 +69,6 @@ class Mover(QWidget):
         d=20
         r=d/2
 
-        self.pointor.x = self.rect().width()/1.5
-        self.pointor.y = self.rect().height()-r
-
         rect = QRectF(self.pointor.x-r, self.pointor.y-r, d, d)
         qp.setBrush(QColor(0,0,0,128))
         qp.drawEllipse(rect)
@@ -65,6 +76,34 @@ class Mover(QWidget):
         
         qp.end()
 
+    def threadFunc(self):
+        while self.bThread:
+            
+            self.attractor.x += 1
+
+            distance = math.sqrt(((self.attractor.x-self.pointor.x)**2)+((self.attractor.y-self.pointor.y)**2))
+            mu = self.attractor.G*self.attractor.mass
+
+            theta = math.atan2(self.attractor.x-self.pointor.x, self.attractor.y-self.pointor.y)
+            degree = theta*(180/math.pi)
+
+            force = (self.attractor.G*self.attractor.mass*self.pointor.mass)/(distance**2)
+            xa = force*math.sin(degree)
+            ya = force*math.cos(degree)
+
+            self.pointor.xspeed -= xa
+            self.pointor.yspeed += ya
+
+            self.pointor.x += self.pointor.xspeed/self.attractor.speed
+            self.pointor.y += self.pointor.yspeed/self.attractor.speed
+
+            self.update()
+            QApplication.processEvents()
+            time.sleep(0.1/self.attractor.speed)
+             
+    def closeEvent(self, e):
+        self.bThread = False
+
 app = QApplication(sys.argv)
-w = Mover()
-sys.exit(app.exec_())   
+w = CWidget()
+sys.exit(app.exec_())
